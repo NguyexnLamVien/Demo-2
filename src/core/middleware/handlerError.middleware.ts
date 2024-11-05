34858e98ff45a8183a40de6aa6e92c06b6d40da6
NguyexnLamVien <nlv.clone@gmail.com>
2024-11-05 08:18:37 +0700

    change erro middleware

---
+++ src/core/middleware/handlerError.middleware.ts
@@ -0,0 +1,12 @@
+import { ErroBase } from '@core/types/error.response';
+import { Request, Response, NextFunction } from 'express';
+
+const errorHandler = (err: ErroBase, req: Request, res: Response, next: NextFunction): void => {
+    if (err instanceof ErroBase) {
+        res.status(err.status).send(err.message);
+    } else {
+        res.status(500).send("An error occurred while processing the request");
+    }
+}
+
+export { errorHandler };
