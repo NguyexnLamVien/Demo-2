55c6e601ded6151f60b592ddac12df31d11f7d11
NguyexnLamVien <nlv.clone@gmail.com>
2024-11-05 08:14:40 +0700

    new declare

--- src/types.d.ts
+++ src/types.d.ts
@@ -5,11 +5,16 @@ declare type WithID = {
 
 
 
-// type Params = {
-//     body?: object,
-//     params?: object,
-//     query?: object,
-//     localObj?: object
-// }
+type Params = {
+    body?: object,
+    params?: object,
+    query?: object,
+    localObj?: object
+}
+declare module Express {
+    type Request = {
+        userId: string
+    }
+}
 
-// declare type ContracterRouter<T extends Params> = import('express').RequestHandler< T['params'] extends undefined ? {} : T['params'], any, T['body'] extends undefined ? {} : T['body'], T['query'] extends undefined ? {} : T['query']>
+declare type ContracterRouter<T extends Params> = import('express').RequestHandler< T['params'] extends undefined ? {} : T['params'], any, T['body'] extends undefined ? {} : T['body'], T['query'] extends undefined ? {} : T['query']>
