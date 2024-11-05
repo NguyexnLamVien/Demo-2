d2e681778ab472c4d2f9999731d5f1146987a223
NguyexnLamVien <nlv.clone@gmail.com>
2024-11-05 08:15:21 +0700

    gán info header

--- src/core/middleware/authentication.middleware.ts
+++ src/core/middleware/authentication.middleware.ts
@@ -1,22 +1,18 @@
-import { Forbidden, Unauthorized } from '@core/types/errorHandler';
+import { Forbidden, Unauthorized } from '@core/types/error.response';
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
+
 const authenMiddleware = (req: any, res: Response, next: NextFunction): void => {
-    const authHeader = req.header('Authorization');
+    const token = req.headers['authorization'];
 
-    if (!authHeader) {
-        throw new Unauthorized("Authorization header missing");
-    }
-
-    const token = authHeader;
     if (!token) {
         throw new Unauthorized('Authorization header missing');
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_TOKEN_SECRET!);
-        req.header = decoded;
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!) as { userId: string };
+        req['userId'] = decoded.userId;
         next();
     } catch (error) {
         throw new Forbidden('Invalid token');
